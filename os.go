57d451aa4b46b7c92015f0d59cd337db496d41f7
xiaowencheng <18379168211.163.com>
2020-06-06 16:44:50 +0800

    init2

--- os.go
+++ os.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	args := os.Args
-	fmt.Println("获得所有输入的参数列表。。。。。")
+	fmt.Println("获得所有输入的参数列表xx。。。。。")
 	for index, value := range args {
 		fmt.Printf("%d ----- %v\n", index, value)
 	}
